fddca1938b1992e483f3a8b86536510779258dd4
Vieenay Siingh <vieenaysiingh@gmail.com>
2016-09-24 22:40:12 +0800

    added endpoints for create, update and delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-     resources :users, only: [:show, :update]
+     resources :users, only: [:show, :create, :update, :destroy]
     end
   end 
 end
